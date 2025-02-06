e1f042b29ca4ab2dd6404f456b924435e783e778
Santiago Florez <herreratiago632@gmail.com>
2025-02-06 13:15:36 -0500

    implemented subtract, multiply and divide method

--- app/calculator.py
+++ app/calculator.py
@@ -2,3 +2,12 @@ class calculator:
 
     def add(self, a,b):
         return a + b
+
+    def subtract(self, a,b):
+        return a - b
+
+    def multiply(self, a,b):
+        return a * b
+
+    def divide(self, a,b):
+        return a / b
